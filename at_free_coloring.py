e4068703ce1ca06e149bc9c47f371a96c2fc9066
Mitsos <dimsintos91@gmail.com>
2023-10-23 10:33:41 +0300

    block-cutpoint decomposition

--- at_free_coloring.py
+++ at_free_coloring.py
@@ -53,6 +53,83 @@ class Graph:
         
         return Graph(new_vertices, new_edges)
     
+    def block_cutpoint_decomposition(self):
+        """
+        Decomposes the graph into its biconnected components.
+
+        Returns:
+            - A list of blocks, where each block is represented as a set of edges.
+            - A set of cutpoints (articulation points).
+        """
+        time = 0  # Discovery time
+        blocks = []  # To store the biconnected components
+        cutpoints = set()
+        
+        # Discovery and low values for each vertex
+        disc = [-1] * len(self.vertices)
+        low = [-1] * len(self.vertices)
+        parent = [-1] * len(self.vertices)
+        visited = {v: False for v in self.vertices}
+        st = []  # An auxiliary stack
+        
+        def block_cutpoint_util(v):
+            print("v:",v)
+            nonlocal time
+            children = 0
+            visited[v] = True
+            disc[v] = time
+            low[v] = time
+            time += 1
+            for u in self.adjacency_list[v]:
+                print("u:",u)
+                # If u is not visited, make v its parent in DFS tree and recurse for it
+                if not visited[u]:
+                    children += 1
+                    parent[u] = v
+                    st.append((v, u))  # Store the edge in stack
+                    block_cutpoint_util(u)
+                    
+                    # Update low value of v
+                    low[v] = min(low[v], low[u])
+                    
+                    # If v is root of DFS tree and has two or more children
+                    if parent[v] == -1 and children > 1:
+                        cutpoints.add(v)
+                        w = -1
+                        block = set()
+                        # Pop all edges from stack till we reach (v, u)
+                        while w != u:
+                            w, x = st.pop()
+                            block.add((w, x))
+                        blocks.append(block)
+                    
+                    # If v is not root and low value of one of its child is more than discovery value of v
+                    elif parent[v] != -1 and low[u] >= disc[v]:
+                        cutpoints.add(v)
+                        w = -1
+                        block = set()
+                        while w != u:
+                            w, x = st.pop()
+                            block.add((w, x))
+                        blocks.append(block)
+                        
+                elif u != parent[v]:
+                    low[v] = min(low[v], disc[u])
+                    print("Mpanei pote edw??", low[v], disc[u])
+                    if disc[u] < disc[v]:
+                        st.append((v, u))
+        
+        for v in self.vertices:
+            if not visited[v]:
+                block_cutpoint_util(v)
+                block = set()
+                while st:
+                    block.add(st.pop())
+                if block:
+                    blocks.append(block)
+                    
+        return blocks, cutpoints
+    
     def show(self):
         net = Network()
         net.add_nodes(self.vertices)
@@ -107,20 +184,26 @@ def line_3_check(graph):
 
 
 # 5-wheel graph
-vertices_5_wheel = ['a','b','c','d','e','f']
-edges_5_wheel = [('a','b'),('a','c'),('a','d'),('b','c'),('c','d'),('b','e'),('c','e'),('c','f'),('d','f'),('e','f')]
+# vertices_5_wheel = ['a','b','c','d','e','f']
+# edges_5_wheel = [('a','b'),('a','c'),('a','d'),('b','c'),('c','d'),('b','e'),('c','e'),('c','f'),('d','f'),('e','f')]
 
-G_5_wheel = Graph(vertices_5_wheel, edges_5_wheel)
+# G_5_wheel = Graph(vertices_5_wheel, edges_5_wheel)
 
-G_5_wheel.show()
+# G_5_wheel.show()
 
-is_condition_met, data = line_3_check(G_5_wheel)
-if is_condition_met:
-    print("data:",data)
-    u, v, common_neighbors = data
-    s = "".join(common_neighbors)
-    new_G_5_wheel = G_5_wheel.contract(common_neighbors)
+# is_condition_met, data = line_3_check(G_5_wheel)
+# if is_condition_met:
+#     print("data:",data)
+#     u, v, common_neighbors = data
+#     s = "".join(common_neighbors)
+#     new_G_5_wheel = G_5_wheel.contract(common_neighbors)
 
 
-    print("IS K4 on recursion:",is_K4_on_recursion(new_G_5_wheel,s))
-    new_G_5_wheel.show()
+#     print("IS K4 on recursion:",is_K4_on_recursion(new_G_5_wheel,s))
+#     new_G_5_wheel.show()
+
+# Testing:
+g = Graph([0, 1, 2, 3, 4, 5], [(0, 1), (1, 2), (0, 2), (2, 3), (3, 4), (4, 5), (3, 5)])
+blocks, cutpoints = g.block_cutpoint_decomposition()
+print("Blocks:", blocks)
+print("Cutpoints:", cutpoints)
